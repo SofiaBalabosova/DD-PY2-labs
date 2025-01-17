6995c0b767b69b13f224ec5417dd0c7c44dee591
SofiaBalabosova <sofabalabosova@gmail.com>
2025-01-17 12:16:59 +0300

    Update task_1.py

--- Лабораторная работа 1/task_1.py
+++ Лабораторная работа 1/task_1.py
@@ -3,46 +3,46 @@ from typing import Union
 import doctest
 
 
-class Bottle:
+class Barrel:
     def __init__(self, capacity_volume: Union[int, float], occupied_volume: Union[int, float]):
         """
-        Создание и подготовка к работе объекта Бутылка
-        :param capacity_volume: Объем бутылки
+        Создание и подготовка к работе объекта Бочка
+        :param capacity_volume: Объем бочки
         :param occupied_volume: Объем занимаемой жидкости
         Примеры:
-        >>> bottle = Bottle(500, 0)  # инициализация экземпляра класса
+        >>> barrel = Barrel(500, 0)  # инициализация экземпляра класса
         """
         if not isinstance(capacity_volume, (int, float)):
             raise TypeError("Объем должен быть типа int или float")
         if capacity_volume <= 0:
             raise ValueError("Объем должен быть положительным числом")
-        self.capacity_volume = capacity_volume  # объем бутылки
+        self.capacity_volume = capacity_volume  # объем бочки
         if not isinstance(occupied_volume, (int, float)):
             raise TypeError("Количество жидкости должно быть int или float")
         if occupied_volume < 0:
             raise ValueError("Количество жидкости не может быть отрицательным числом")
-        self.occupied_volume = occupied_volume  # занятый объем бутылки
+        self.occupied_volume = occupied_volume  # занятый объем бочки
 
-    def is_full_bottle(self) -> bool:
+    def is_full_barrel(self) -> bool:
         """
-        Функция которая проверяет является ли бутылка полной
+        Функция которая проверяет является ли бочка полной
 
-        :return: Является ли бутылка полной
+        :return: Является ли бочка полной
 
         Примеры:
-        >>> bottle = Bottle(500, 0)
-        >>> bottle.is_full_bottle()
+        >>> barrel = Barrel(500, 0)
+        >>> barrel.is_full_barrel()
         """
-    def add_water_to_bottle(self, water: float) -> None:
+    def add_water_to_barrel(self, water: float) -> None:
         """
-        Добавление воды в бутылку.
+        Добавление воды в бочку.
         :param water: Объем добавляемой жидкости
 
-        :raise ValueError: Если количество добавляемой жидкости превышает свободное место в бутылке, то вызываем ошибку
+        :raise ValueError: Если количество добавляемой жидкости превышает свободное место в бочке, то вызываем ошибку
 
         Примеры:
-        >>> bottle = Bottle(500, 0)
-        >>> bottle.add_water_to_bottle(200)
+        >>> barrel = Barrel(500, 0)
+        >>> barrel.add_water_to_barrel(200)
         """
         if not isinstance(water, (int, float)):
             raise TypeError("Добавляемая жидкость должна быть типа int или float")
@@ -50,87 +50,86 @@ class Bottle:
             raise ValueError("Добавляемая жидкость должна быть положительным числом")
 
 
-class Cat:
-    def __init__(self, name: str):
+class Table:
+    def __init__(self, material: str):
         """
-        Создание и подготовка к работе объекта Собака
-        :param name: Имя кошки
-        :param tricks: список трюков, которым обучена кошка
+        Создание и подготовка к работе объекта Стол
+        :param material: Материал стола
+        :param height: Высота стола
         Примеры:
-        >>> cat = Cat('Пушок')  # инициализация экземпляра класса
+        >>> table = Table('Дуб')  # инициализация экземпляра класса
         """
-        self.name = name
-        self.tricks = []    # создание пустого списка для каждой собаки
+        self.material = material
+        self.height = []    # создание пустого списка для высоты стола
 
-    def add_trick(self, trick: str) -> None:
+    def add_height(self, height: str) -> None:
         """
-        Добавление трюков для кошки.
-        :param trick: название трюка
+        Добавление высоты стола.
+        :param height: высота
         Примеры:
-        >>> cat = Cat('Пушок')
-        >>> cat.add_trick('Голос')
+        >>> table = Table('Дуб')
+        >>> table.add_height('120 см')
         """
 
-    def check_trick(self, trick: str) -> bool:
+    def check_height(self, height: str) -> bool:
         """
-        Функция которая проверяет обучена ли кошка трюку
+        Функция, которая проверяет, есть ли нужная высота стола
 
-        :return: Знает ли кошка трюк
+        :return: Есть ли стол нужной высоты
 
         Примеры:
-        >>> cat = Cat('Пушок')
-        >>> cat.check_trick('Перевернись')
+        >>> table = Table('Дуб')
+        >>> table.check_height('100 см')
         """
 
 
-class Box:
-    def __init__(self, products: str, capacity_volume: Union[int, float], occupied_volume: Union[int, float]):
+class Flashdrive:
+    def __init__(self, files: str, capacity_volume: Union[int, float], occupied_volume: Union[int, float]):
         """
-        Создание и подготовка к работе объекта Ящик
-        :param products: вид продукции в ящике
-        :param capacity_volume: вместимость ящика
-        :param occupied_volume: количество продукции в ящике
+        Создание и подготовка к работе объекта Флешка
+        :param files: вид файлов на флешке
+        :param capacity_volume: объем памяти на флешке
+        :param occupied_volume: объем файлов на флешке
         Примеры:
-        >>> box = Box('яблоки',500, 0)  # инициализация экземпляра класса
+        >>> flashdrive = Flashdrive('картинки',128, 0)  # инициализация экземпляра класса
         """
-        self.products = products    # вид продукции на складе
+        self.files = files    # вид файлов на флешке
         if not isinstance(capacity_volume, (int, float)):
-            raise TypeError("Вместимость должна быть типа int или float")
+            raise TypeError("Объем памяти на флешке должен быть типа int или float")
         if capacity_volume <= 0:
-            raise ValueError("Вместимость должна быть положительным числом")
-        self.capacity_volume = capacity_volume  # вместимость ящика
+            raise ValueError("Объем памяти на флешке должен быть положительным числом")
+        self.capacity_volume = capacity_volume  # объем памяти на флешке
         if not isinstance(occupied_volume, (int, float)):
-            raise TypeError("Количество продукции на складе должно быть int или float")
+            raise TypeError("Объем файлов на флешке должен быть int или float")
         if occupied_volume < 0:
-            raise ValueError("Количество продукции на складе не может быть отрицательным числом")
-        self.occupied_volume = occupied_volume  # количество продукции в ящике
+            raise ValueError("Объем файлов на флешке не может быть отрицательным числом")
+        self.occupied_volume = occupied_volume  # объем файлов на флешке
 
-    def is_empty_box(self) -> bool:
+    def is_empty_flashdrive(self) -> bool:
         """
-        Функция которая проверяет является ли ящик свободным
+        Функция которая проверяет является ли флешка пустой
 
-        :return: Является ли ящик свободным
+        :return: Является ли флешка пустой
 
         Примеры:
-        >>> box = Box('яблоки',500, 0)
-        >>> box.is_empty_box()
+        >>> flashdrive = Flashdrive('картинки',128, 0)
+        >>> flashdrive.is_empty_flashdrive()
         """
-    def add_products_to_box(self, new_product: str, volume: Union[int, float]) -> None:
+    def add_files_to_flashdrive(self, new_file: str, volume: Union[int, float]) -> None:
         """
-        Заполнение ящика.
-        :param new_product: тип продукции
+        Заполнение флешки.
+        :param new_file: тип файла
 
-        :raise ValueError: Если количество добавляемой продукции превышает свободное место в ящике, то вызываем ошибку
-        :raise TypeError: Если новая продукция не совпадает с находящейся в ящике
+        :raise ValueError: Если объем добавляемых файлов превышает свободное место на флешке, то вызываем ошибку
 
         Примеры:
-        >>> box = Box('яблоки',500, 0)
-        >>> box.add_products_to_box('яблоки', 200)
+        >>> flashdrive = Flashdrive('картинки',128, 0)
+        >>> flashdrive.add_files_to_flashdrive('картинки', 16)
         """
         if not isinstance(volume, (int, float)):
-            raise TypeError("Количество добавляемой продукции должно быть типа int или float")
+            raise TypeError("Объем добавляемых файлов должен быть типа int или float")
         if volume < 0:
-            raise ValueError("Количество добавляемой продукции должно быть положительным числом")
+            raise ValueError("Объем добавляемых файлов должен быть положительным числом")
 
 
 if __name__ == "__main__":
